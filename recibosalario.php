6faa9af548ea8e64c4830456b188afab00596095
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-05-06 20:55:11 -0300

    Para onde vai?

--- recibosalario.php
+++ recibosalario.php
@@ -2,35 +2,41 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <link rel="stylesheet" href="./style.css">
 
 <body class=box>
-  
-  <?php
-  $valor = $_POST("valorInput");
-  $empregado = $_POST("empregadoInput");
-  $aTitulo = $_POST("aTituloInput");
-  $empregador = $_POST("empregadorInput");
-  $cpfCnpj = $_POST("cpfCNPJInput");
-  $cidade = $_POST("cidadeInput");
-  $data = $_POST("dataInput");
-  $formasPagamento = $_POST("radio");
-  $remuneracaoMensal = $_POST("remuneracaoMensalInput");
 
-  $variaveis='
-  <p>"Eu, <b> '.$empregado' </b> declaro para todos os efeitos, ter recebido a título de <b> '.$aTitulo' </b>, a importância de: <b> R$'.$valor',00 </b>, e em concordância com o disposto no <b> Art. 462, da CLT </b>, tenho plena consciência de que o respectivo valor será descontado, pelo empregador: <b> '.$empregador' </b>, de CPF/CNPJ <b> Nº '.$cpfCnpj' </b>, do pagamento da minha remuneração mensal relativa à folha do mês de <b>'.$remuneracaoMensal</b><p>.;
-
-echo "$variaveis";
-
-?>
-  <div style="padding-left:300px" ;>
-   <input type="hidden" name="gerarRecibo">
-   <button type="submit" class="submit" onclick="imprimir()">
-  </div>
-   
-</body>
-</html>
+  <div id="reciboImprimir">
+    <div id="button">
+      <p>
+        <input type="hidden" name="gerarRecibo">
+        <button type="submit" class="submit" onclick="voltar()">
+          <span>Voltar</span>
+        </button>
+      </p>
+      <div id="reciboPraImprimir" class="bordaRecibo">
+        <h1>Recibo Salarial</h1>
+        <div>
+          <label class="aoLado" id="primeiraVia">
+            <h3>1º Via</h3>
+          </label>
+          <label id="valorLabel"><b class="aoLado" id="valorPago"></b></label>
+        </div>
+        <div>
+          <p id="texto2" class="texto"></p>
+          <p id="texto3" class="texto"> Eu, <b><?=$_POST['empregadoInput']?></b>Incidindo na base de cálculo de encargos sociais e fiscais para todos os efeitos legais.</p>
+          <br>
+          <p id="texto4" class="texto">Ass:</p>
+          <p id="texto5" class="texto"> ―――――――――――――――――――― </p>
+        </div>
+        <div>
+          <p id="dataImpressao"></p>
+        </div>
+      </div>
+    </div>
+    <script type="text/javascript" src="script.js"></script>
+    </form>
