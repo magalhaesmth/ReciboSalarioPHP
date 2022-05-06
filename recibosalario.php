94abc1dc292ed9fe988ad9d7bfb2db2406e50538
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-05-06 15:14:17 -0300

    Inserindo

--- recibosalario.php
+++ recibosalario.php
@@ -1,6 +1,26 @@
 <!DOCTYPE html>
-<html lang="utf-8">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Base</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+<link rel="stylesheet" href="./style.css">
+
+<body>
+  
+  <?php
+  $nomeCredor = $_POST['nomeDoCredor'];
+  $valor = $_POST("valorInput");
+  $empregado = $_POST("empregadoInput");
+  $aTitulo = $_POST("aTituloInput");
+  $empregador = $_POST("empregadorInput");
+  $cpfCnpj = $_POST("cpfCNPJInput");
+  $cidade = $_POST("cidadeInput");
+  $data = $_POST("dataInput");
+  $formasPagamento = $_POST("radio");
+  $remuneracaoMensal = $_POST("remuneracaoMensalInput");
+
+
+?>
